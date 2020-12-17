18bf5ff4fb683baf431e8d9a5ce99755e6520ca1
PaulsBecks <paul.beck.berlin@gmail.com>
2020-12-17 14:16:08 +0100

    externalize apropriate function add add error as return value

--- main.go
+++ main.go
@@ -114,88 +114,92 @@ func createKitty(ctx contractapi.TransactionContextInterface, matronID, sireID, 
 	return newKittenID, nil
 }
 
-func _owns(kittyID uint64, owner string) bool {
-	return true
+func owns(kittyID uint64, owner string) (bool, error) {
+	return false, nil
 }
 
-func _approvedFor(kittyId uint64, account string) bool {
-	return true
+func approvedFor(kittyId uint64, account string) (bool, error) {
+	return false, nil
 }
 
-func _approve(kittdyId uint64, account string) {
-	// Emit Approval
-	return
+func approve(kittdyId uint64, account string) error {
+	return nil
 }
 
-func transferFrom(from, to string, kittyId uint64) {
-	return
+func (c *KittyContract) Transfer(ctx contractapi.TransactionContextInterface, to string, kittyID uint64) error {
+	return nil
 }
 
-func totalSuppy() uint64 {
+func (c *KittyContract) Approve(ctx contractapi.TransactionContextInterface, kittdyId uint64, account string) error {
+	return nil
+}
+
+func (c *KittyContract) TotalSuppy(ctx contractapi.TransactionContextInterface) uint64 {
 	return 0
 }
 
-func ownerOf(kittyId uint64) string {
-	return ""
+func (c *KittyContract) OwnerOf(ctx contractapi.TransactionContextInterface, kittyId uint64) (string, error) {
+	return "", nil
 }
 
-func tokensOfOwner(owner string) []uint64 {
-	return []uint64{}
+func (c *KittyContract) TokensOfOwner(owner string) ([]uint64, error) {
+	return []uint64{}, nil
 }
 
-func pregnantKitties() uint64 {
-	return 0
+func (c *KittyContract) PregnantKitties() (uint64, error) {
+	return 0, nil
 }
 
-func isReadyToBreed(kittyId uint64) bool {
-	return true
+func isReadyToGiveBirth(matron Kitty) (bool, error) {
+	return true, nil
 }
 
-func isSiringPermitted(kittyId uint64) bool {
-	return true
+func isReadyToBreed(kittyId uint64) (bool, error) {
+	return false, nil
 }
 
-func triggerCooldown(kittyId uint64) {
-	return
+func (c *KittyContract) IsReadyToBreed(kittyId uint64) (bool, error) {
+	return false, nil
 }
 
-func approveSiring(kittyId uint64, siringPartner string) {
-	return
+func isSiringPermitted(kittyId uint64) (bool, error) {
+	return true, nil
 }
 
-func isReadyToGiveBirth(kittyId uint64) bool {
-	return true
+func triggerCooldown(kittyId uint64) error {
+	return nil
 }
 
-func IsReadyToBreed(ctx contractapi.TransactionContextInterface, kittyId uint64) bool {
-	return true
+func (c *KittyContract) ApproveSiring(ctx contractapi.TransactionContextInterface, kittyId uint64, siringPartner string) error {
+	return nil
 }
 
-func isPregnant(kittyId uint64) bool {
-	return true
+func isPregnant(kittyId uint64) (bool, error) {
+	return false, nil
 }
 
-func isValidMatingPair(sire uint64, matron uint64) bool {
-	return true
+func isValidMatingPair(sire uint64, matron uint64) (bool, error) {
+	return true, nil
 }
 
-func canBreedWith(sire uint64, matron uint64) bool {
-	return true
+func canBreedWith(sire uint64, matron uint64) (bool, error) {
+	return true, nil
 }
 
-func breedWith(sire uint64, matron uint64) {
-	return
+func breedWith(sire uint64, matron uint64) error {
+	return nil
 }
 
-func BreedWithAuto(ctx contractapi.TransactionContextInterface, sire uint64, matron uint64) {
-	return
+func (c *KittyContract) BreedWithAuto(ctx contractapi.TransactionContextInterface, sireID uint64, matronID uint64) error {
+	return nil
 }
 
-func GiveBirth(ctx contractapi.TransactionContextInterface, kittyId uint64) {
-	return
+func (c *KittyContract) GiveBirth(ctx contractapi.TransactionContextInterface, matronID uint64) error {
+	return nil
 }
 
-func (c *KittyContract) Transfer(ctx contractapi.TransactionContextInterface, from, to string, kittyID uint64) error {
+func (c *KittyContract) TransferFrom(ctx contractapi.TransactionContextInterface, from, to string, kittyID uint64) error {
+	// TODO (hb237): extend the functionality of this function to match the ethereum implementation
 	return transfer(ctx, from, to, kittyID)
 }
 
